37df3fe0393466fe6bf65e44a363f4b041f05f38
Seol Cheon <jojo432143@gmail.com>
2020-06-28 16:36:58 -0400

    app.js and app.css

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+//IIFE - Immediately Invoked Function Expressions.
+(function(){
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+
+    window.addEventListener("load", Start);
+
+}())
