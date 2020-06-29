3b15d8e68bcdf41b42e3361c5e85610feae7a29a
Seol Cheon <jojo432143@gmail.com>
2020-06-29 16:50:02 -0400

    added nav link highlighting

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,33 @@
     function Start()
     {
         console.log("App Started...");
+        let title = document.title;
+        console.log(`The title of the page is ${title}`);
+
+        title = title.toLowerCase();
+
+        let navAnchors = document.querySelectorAll("li a");
+
+        for (const anchor of navAnchors) {
+
+            let anchorString =anchor.getAttribute("href")
+            anchorString = anchorString.substr(0,anchorString.length -5);
+            
+            if((title==="about")&&(anchorString==="index")||(title===anchorString))
+            {
+                anchor.className="nav-link active";
+            }            
+        }
+        //step 1 hook into the spot {element} on the page
+        let jumbotron = document.getElementsByClassName("jumbotron")[0];
+        //step 2 create a new element
+        let neweParagraph = document.createElement("p");
+        //step 3 configure the new element
+        neweParagraph.textContent =title;
+        //step 4 attach the new element
+        jumbotron.appendChild(neweParagraph);
+
+        
     }
 
     window.addEventListener("load", Start);
