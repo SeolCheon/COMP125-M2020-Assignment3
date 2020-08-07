6999b65a9ca1bf6a4b8553aa0f9296f30c555d26
Seol Cheon <jojo432143@gmail.com>
2020-08-07 01:08:34 -0400

    Header and footer injected

--- Scripts/app.js
+++ Scripts/app.js
@@ -27,6 +27,7 @@
         }     
         return title;  
     }
+
     //add texts in about me page
     function addParagraphsToAboutmeJumbotron() {
         //step 1 hook into the spot {element} on the page
@@ -48,9 +49,7 @@
             return true;
         }return false;        
        
-    } 
-
-   
+    }    
 
 
     //add texts in projects page
@@ -59,28 +58,21 @@
         
         if (projectsjumbotron) //if jumbotron exists
         {
-            //create a list
-            let list1 = document.createElement("li");
-            //step 3 configure the new element
-            list1.textContent =
-                `
-                Building a Korean Digital Art Gallery.
-                    
-                `;
-            //attatch list
-            projectsjumbotron.appendChild(list1);
-
+           
             //create div element
             let firstParagraph = document.createElement("div");            
             firstParagraph.innerHTML =
-                `                
+                `   
+                    Building a Korean Digital Art Gallery.
+
                     I like to draw pictures about my culture!
                     I'm from Korea and I think there's so many amazing architectures
                     in my country. And I really want to promote those!
                     But It is a bit hard to travel to Korea everytime you want to see works.
                     So Finally, I want to build website for digital art gallery 
                     where there are a lot of beautiful picutres of korean cultures that I draw.     
-                    FYI, a picture above is my work!           
+                    FYI, a picture above is my work!     
+
                     
                 `; 
             //attach new div
@@ -89,26 +81,21 @@
             let line = document.createElement("hr");                    
             projectsjumbotron.appendChild(line);
 
-            //create a list
-            let list2 = document.createElement("li");
-            //configure element
-            list2.textContent =
-                `
-                Making a language learning App.
-                `; 
-            //attatch the element    
-            projectsjumbotron.appendChild(list2);   
-            //create the div element        
+
+            //create div element
             let secondParagraph = document.createElement("div");
             //configure the div element
             secondParagraph.innerHTML =
                 `
+                Making a language learning App.
+
                 I like to learn langauge with language learning App.
                 I want to make the App with AI.
                 Not only can this AI translate what people says with their language,
                 Also this AI can recognize user's voice and analyze the accent and pronunciation
                 and It will give feedback to users so they can fix their langauge speaking. 
-                I'll charge of analyzing speaking patterns.               
+                I'll charge of analyzing speaking patterns.
+
     
                 `;         
             //attatch the element   
@@ -118,28 +105,21 @@
             //attatch hr element                   
             projectsjumbotron.appendChild(line2);
             //create li element
-            let list3 = document.createElement("li");  
-            //configure it         
-            list3.textContent =
-                `
-                Making a YouTube channel!
-                    
-                `; 
-            // attach the new element
-            projectsjumbotron.appendChild(list3);
+
 
             //create new element div
             let thirdParagraph = document.createElement("div");
             //configure
             thirdParagraph.innerHTML =
                 `
+                Making a YouTube channel!
+
                 I like to share my ideas and knowledge with people.
                 I think becoming a youtuber will help me improve my English
                 and learn skills for buiding websites using language!
                 The reason why I think like this is I believe that only after I teach people 
                 with my knowledge, that knowledge is real mine now.
-                So My last project that I haven't started yet eagerly is to become a Youtuber! 
-                                   
+                So My last project that I haven't started yet eagerly is to become a Youtuber!                                    
     
                 `; 
             //attatch element div               
@@ -211,34 +191,70 @@
         }        
         return false;
     }
+
+
+    
+
+    function loadHeader()
+    {
+        console.info("Header Loading...");
+
+        // step 1 - creates the XHR object
+        let XHR = new XMLHttpRequest();
+
+        // step 2 - configures the message
+        XHR.open("GET", "./Views/partials/header.html");
+
+        // step 3 - Executes the request
+        XHR.send();
+
+        XHR.addEventListener("readystatechange", function(){
+            if((XHR.readyState === 4) && (XHR.status === 200))
+            {
+                let header = document.getElementsByTagName("header")[0];
+
+                let headerData = XHR.responseText;
+
+                header.innerHTML = headerData;                
+            }
+        });
+    }
+
+    function loadFooter()
+    {
+        console.info("Footer Loading...");
+
+        // step 1 - creates the XHR object
+        let XHR = new XMLHttpRequest();
+
+        // step 2 - configures the message
+        XHR.open("GET", "./Views/partials/footer.html");
+
+        // step 3 - Executes the request
+        XHR.send();
+
+        XHR.addEventListener("readystatechange", function(){
+            if((XHR.readyState === 4) && (XHR.status === 200))
+            {
+                let footer = document.getElementsByTagName("footer")[0];
+
+                let footerData = XHR.responseText;
+
+                footer.innerHTML = footerData;
+            }
+        });
+    }
     
     //when page loaded it happens
     function Start()
     {
         console.log("App Started...");
+        loadHeader();
+        loadFooter();
 
         let title = highlightActiveLink();
-        //check if sucessfully paragraphs added in about me page
-         let success =addParagraphsToAboutmeJumbotron(); 
-        if(success)
-        {
-        console.log("successfully added paragraphs to index.html jumbotron");
-        }
-        else
-        {
-        console.log("content not added to index.html jumbotron - does not exist"); 
-        } 
-
-        //check if sucessfully paragraphs added in projects page
-        let success1 =addParagraphsToProjectsJumbotron(); 
-        if(success1)
-        {
-        console.log("successfully added paragraphs to projects.html jumbotron");
-        }
-        else
-        {
-        console.log("content not added to projects.html jumbotron - does not exist"); 
-        }
+        
+         
         //check if sucessfully form validated
         let formValidated =validateForm();
         if(formValidated)
